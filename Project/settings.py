315f11d3e7632e8fc005eb270cf33bd62e0b5b47
shivamkumraa9 <shivamkumraa9@gmail.com>
2020-07-19 20:42:38 +0530

    Fixed

--- Project/settings.py
+++ Project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -137,3 +137,5 @@ EMAIL_HOST_PASSWORD = 'fd5saf5asdf5as#%$'
 
 LOGIN_URL = "users:login"
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+django_heroku.settings(locals())

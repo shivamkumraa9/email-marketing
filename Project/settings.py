a3f6141b87a1b2a12e11a05a0652d5f7b53cbb7a
shivamkumraa9 <shivamkumraa9@gmail.com>
2020-07-19 20:28:54 +0530

    Version 1

--- Project/settings.py
+++ Project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&eq!pu(r+--w&tq596@5bk@mn&ud(7li-$cj3+gyyhi%f_c)el'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 

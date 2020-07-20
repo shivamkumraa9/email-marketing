681d977f64704e08e636d53993faf54e2b2dcee7
shivamkumraa9 <shivamkumraa9@gmail.com>
2020-07-20 22:45:10 +0530

    Final Deploy Done

--- Project/settings.py
+++ Project/settings.py
@@ -131,8 +131,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'extraservices05@gmail.com'
-EMAIL_HOST_PASSWORD = 'itsgmail#'
+EMAIL_HOST_USER = os.environ['EMAIL']
+EMAIL_HOST_PASSWORD = os.environ['PASSWORD']
 
 
 
